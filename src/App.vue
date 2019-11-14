4e8f8ee41dc0a05dc7f209738a59d90ff104979d
Jose Alarcon <joseluis_alarcon@outlook.com>
2019-11-14 15:21:56 -0700

    home v1 working on reviews page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,85 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <v-app>
+    <v-app-bar
+      app
+      elevate-on-scroll
+      color="transparent"
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn large text>shop
+      </v-btn>
+      <v-btn @click="$router.push('reviews')" large text>
+          Reviews
+      </v-btn>
+    </v-app-bar>
+
     <router-view/>
-  </div>
+
+    <v-footer padless absolute style="padding-top: 15px;">
+        <v-card
+      flat
+      tile
+      class="orange lighten-1 white--text text-center"
+    >
+      <v-card-text>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
+        >
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-0">
+        Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+          <v-icon>mdi-copyright</v-icon>
+        {{ new Date().getFullYear() }} Swingshot, LLC. All rights reserved.
+      </v-card-text>
+    </v-card>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: 'App',
+  data: () => ({
+      icons: [
+        'fab fa-facebook',
+        'fab fa-twitter',
+        'fab fa-google-plus',
+        'fab fa-linkedin',
+        'fab fa-instagram',
+      ],
+    }),
+};
+</script>
---
+++ src/views/Reviews.vue
@@ -0,0 +1,65 @@
+<template>
+    <div id="reviews" style="margin-top: 90px;">
+        <div style="display:flex;">
+            <div style="flex-flow:row; justify-content:space-around;">
+                <!-- <div>
+                    <h3>Swingshot <br>reviews: Hear<br> from our<br> customers</h3>
+                    <v-divider></v-divider>
+                    <v-rating v-model="totalRating"></v-rating>
+                    <div id="player"></div>
+                </div>
+                <div>
+                    <div>ok</div>
+                </div> -->
+                <div>OK</div>
+                <div>GET</div>
+            </div>
+        </div>
+        <!-- <v-row>
+            <v-col cols="12">
+                <v-row align="center" justify="center">
+                    <div>
+                        <h3>Swingshot <br>reviews: Hear<br> from our<br> customers</h3>
+                        <v-divider></v-divider>
+                        <v-rating v-model="totalRating"></v-rating>
+                    </div>
+                </v-row>
+            </v-col>
+        </v-row> -->
+            <!-- <h1>ok</!-->
+    </div>
+</template>
+<script src='https://www.youtube.com/iframe_api' async></script>
+<script>
+export default {
+    name: "reviews",
+    data(){
+        return {
+            allReviews: [
+                {
+                    date: new Date(),
+                    stars: 5,
+                    reviewerName: "Bart May",
+                    source: "Site",
+                    title: "Best Hand Thrower Ever Made",
+                    reviewText: "This is by far the best hand held pigeon thrower I have ever used."
+                },
+                {
+                    date: new Date(2011-11-11),
+                    stars: 4,
+                    reviewerName: "Lisa Lampanelli",
+                    source: "Amazon",
+                    title: "Meh, it's good",
+                    reviewText: "My 9-yea-old is too small for it"
+                }
+            ],
+            totalRating: 4.5
+        }
+    },
+    mounted(){
+        const YTPlayer = require('yt-player');
+        const player = new YTPlayer('#player');
+        player.load('M3AbMA1VUis');
+    }
+}
+</script>
